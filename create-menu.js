366b10d5fddacbf3411cb48b59da610a2090e670
priyankanaik625 <76041822+priyankanaik625@users.noreply.github.com>
2020-12-29 07:34:42 +0100

    added callback function

--- create-menu.js
+++ create-menu.js
@@ -1,41 +1,41 @@
-fetch('./data.json')
-    .then(results => results.json())
-    .then(data => appendData(data))
-    .catch(err => console.log('Error:' + err));
+// fetch('./data.json')
+//     .then(results => results.json())
+//     .then(data => appendData(data))
+//     .catch(err => console.log('Error:' + err));
 
-const appendData = (data) => {
-    const menuNavigation = document.getElementById("menu__nav");
+// const appendData = (data) => {
+//     const menuNavigation = document.getElementById("menu__nav");
 
-    var temp = '';
-    temp += '<ul class="menu__list-level1" role="menubar">'
+//     var temp = '';
+//     temp += '<ul class="menu__list-level1" role="menubar">'
 
-    if(data.level1) {
-        data.level1.forEach(item => {
-            temp += '<li class="menu__item-level1" role="none">'
-            temp += '<a class="menu__link-level1" role="menuitem" href='+item.url+' >' + item.name + '</a>'
-            temp += '<button class="menu__toggle"><i>⌄</i></button>'
-            temp += '<ul class="menu__list-level2" role="menubar">'
+//     if(data.level1) {
+//         data.level1.forEach(item => {
+//             temp += '<li class="menu__item-level1" role="none">'
+//             temp += '<a class="menu__link-level1" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+//             temp += '<button class="menu__toggle"><i>⌄</i></button>'
+//             temp += '<ul class="menu__list-level2" role="menubar">'
 
-            if(item.level2) {
-                item.level2.forEach(item => {
-                    temp += '<li class="menu__item-level2" role="none">'
-                    temp += '<a class="menu__link-level2" role="menuitem" href='+item.url+' >' + item.name + '</a>'
-                    temp += '<button class="menu__toggle"><i>⌄</i></button>'
-                    temp += '<ul class="menu__list-level3" role="menubar">'
+//             if(item.level2) {
+//                 item.level2.forEach(item => {
+//                     temp += '<li class="menu__item-level2" role="none">'
+//                     temp += '<a class="menu__link-level2" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+//                     temp += '<button class="menu__toggle"><i>⌄</i></button>'
+//                     temp += '<ul class="menu__list-level3" role="menubar">'
                     
-                    if(item.level3) {
-                        item.level3.forEach(item => {
-                            temp += '<li class="menu__item-level3">'
-                            temp += '<a class="menu__link-level3" role="menuitem" href='+item.url+' >' + item.name + '</a>'
-                        });
-                        temp += '</ul></li>'
-                    }
-                });
-                temp += '</ul></li>'
-            }
-        });
-    }
+//                     if(item.level3) {
+//                         item.level3.forEach(item => {
+//                             temp += '<li class="menu__item-level3">'
+//                             temp += '<a class="menu__link-level3" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+//                         });
+//                         temp += '</ul></li>'
+//                     }
+//                 });
+//                 temp += '</ul></li>'
+//             }
+//         });
+//     }
 
-    temp += '</ul>'
-    menuNavigation.innerHTML = temp;
-}
+//     temp += '</ul>'
+//     menuNavigation.innerHTML = temp;
+// }
--- menu.js
+++ menu.js
@@ -1,3 +1,45 @@
+fetch('./data.json')
+    .then(results => results.json())
+    .then(data => appendData(data, accordion))
+    .catch(err => console.log('Error:' + err))
+
+const appendData = (receivedData, callback) => {
+    const menuNavigation = document.getElementById("menu__nav");
+
+    var temp = '';
+    temp += '<ul class="menu__list-level1" role="menubar">'
+
+    if(receivedData.level1) {
+        receivedData.level1.forEach(item => {
+            temp += '<li class="menu__item-level1" role="none">'
+            temp += '<a class="menu__link-level1" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+            temp += '<button class="menu__toggle"><i>⌄</i></button>'
+            temp += '<ul class="menu__list-level2" role="menubar">'
+
+            if(item.level2) {
+                item.level2.forEach(item => {
+                    temp += '<li class="menu__item-level2" role="none">'
+                    temp += '<a class="menu__link-level2" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+                    temp += '<button class="menu__toggle"><i>⌄</i></button>'
+                    temp += '<ul class="menu__list-level3" role="menubar">'
+                    
+                    if(item.level3) {
+                        item.level3.forEach(item => {
+                            temp += '<li class="menu__item-level3">'
+                            temp += '<a class="menu__link-level3" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+                        });
+                        temp += '</ul></li>'
+                    }
+                });
+                temp += '</ul></li>'
+            }
+        });
+    }
+
+    temp += '</ul>'
+    menuNavigation.innerHTML = temp;
+    callback();
+}
 
 const menu = document.querySelector(".menu");
 const drawer = document.querySelector('.info__drawer');
@@ -10,7 +52,7 @@ const getSiblings = elem => {
 };
 
 //menu toggles/accordion
-setTimeout( () => { 
+accordion = () => {
     const toggleButtons = menu.querySelectorAll(".menu__toggle");
     toggleButtons.forEach(toggle => {
         toggle.addEventListener('click', () => {
@@ -41,7 +83,8 @@ setTimeout( () => {
 
         });
     }); 
-}, 100);
+}
+
 
 //Operations on Drawer
 drawer.addEventListener('click', () => {
