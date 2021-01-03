866d65ac4fb378d0575d40ca54fdbc62884080b0
priyankanaik625 <76041822+priyankanaik625@users.noreply.github.com>
2021-01-03 06:19:00 +0100

    Added aria-haspopup and template-literals

--- create-menu.js
+++ create-menu.js
@@ -8,36 +8,36 @@ const appendData = (receivedData, callback) => {
     const menuNavigation = document.getElementById("menu__nav");
 
     var temp = '';
-    temp += '<ul class="menu__list-level1" role="menubar">'
+    temp += `<ul class="menu__list-level1" role="menubar">`
 
     if(receivedData.navigation) {
         receivedData.navigation.forEach(item => {
-            temp += '<li class="menu__item-level1" role="none">'
-            temp += '<a class="menu__link-level1" role="menuitem" href='+item.url+' >' + item.name + '</a>'
-            temp += '<button class="menu__toggle"><i>⌄</i></button>'
-            temp += '<ul class="menu__list-level2" role="menubar">'
+            temp += `<li class="menu__item-level1" role="none">`
+            temp += `<a class="menu__link-level1" role="menuitem" aria-haspopup="true" href=${item.url}> ${item.name} </a>`
+            temp += `<button class="menu__toggle"><i>⌄</i></button>`
+            temp += `<ul class="menu__list-level2" role="menubar" aria-hidden="true" aria-label="${item.name} submenu">`
             
             if(item.subnav) {
                 item.subnav.forEach(item => {
-                    temp += '<li class="menu__item-level2" role="none">'
-                    temp += '<a class="menu__link-level2" role="menuitem" href='+item.url+' >' + item.name + '</a>'
-                    temp += '<button class="menu__toggle"><i>⌄</i></button>'
-                    temp += '<ul class="menu__list-level3" role="menubar">'
+                    temp += `<li class="menu__item-level2" role="none">`
+                    temp += `<a class="menu__link-level2" role="menuitem" aria-haspopup="true" href=${item.url}> ${item.name} </a>`
+                    temp += `<button class="menu__toggle"><i>⌄</i></button>`
+                    temp += `<ul class="menu__list-level3" role="menubar" aria-hidden="true" aria-label="${item.name} submenu">`
                     
                     if(item.subnav) {
                         item.subnav.forEach(item => {
-                            temp += '<li class="menu__item-level3">'
-                            temp += '<a class="menu__link-level3" role="menuitem" href='+item.url+' >' + item.name + '</a>'
+                            temp += `<li class="menu__item-level3">`
+                            temp += `<a class="menu__link-level3" role="menuitem" href=${item.url} > ${item.name} </a>`
                         });
-                        temp += '</ul></li>'
+                        temp += `</ul></li>`
                     }
                 });
-                temp += '</ul></li>'
+                temp += `</ul></li>`
             }
         });
     }
 
-    temp += '</ul>'
+    temp += `</ul>`
     menuNavigation.innerHTML = temp;
     if(callback) callback();
 }
